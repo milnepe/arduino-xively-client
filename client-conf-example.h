fb661214f3198ba3b76342fd29de7db7fa8120a7
Pete Milne <milnepe@gmail.com>
2017-07-11 19:34:46 +0100

    start version 0.4

---
+++ client-conf-example.h
@@ -0,0 +1,21 @@
+/*
+
+ client-conf-example.h
+
+  Author: Pete Milne
+  Date: 11-07-2017
+  Version: 0.4
+
+ Example Xively Client Config file
+ Add own credentials and rename to client-conf.h
+
+*/
+
+// Update interval milli seconds
+#define UPDATE_INTERVAL 300000
+
+// Xively account details
+#define APIKEY         "ABC1234567890XYZ" //Your key
+#define FEEDID         123456 // Your feed ID
+#define USERAGENT      "Temperature Sensors" // Your project name
+
